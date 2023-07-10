79dceeedd04dffa1639bbe378b62bc13efa76d57
arifaniyas <arifaniyas.tn@gmail.com>
2023-07-10 12:36:11 +0400

    first

---
+++ app.js
@@ -0,0 +1,16 @@
+function getData(){
+    var url = document.getElementById('url').value;
+    
+       fetch(url,{
+           method: 'GET'
+       })
+       .then(response => response.json())
+       .then(data => {
+           document.getElementById('jsonData').innerHTML = JSON.stringify(data);
+           document.getElementById('jsonData').innerHTML ="<pre>" + JSON.stringify(data,undefined, 4) + " </pre>";
+       })
+       .catch(error => {
+    document.getElementById('error').innerHTML = error;
+       })
+    
+    }
